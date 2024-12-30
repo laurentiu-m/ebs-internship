cf40f2af68138420c8197954d559e9899c20c072
Laurentiu Molesteanu <laurentiu.molesteanu@internal.ebs.md>
2024-12-30 17:39:08 +0200

    feat: add route to register

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,8 +1,9 @@
 import { Router, Request, Response } from "express";
 import jsonServer from "json-server";
-import { User, LoginUser } from "../types";
+import { User, LoginUser, RegisterUser } from "../types";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
+import { axiosInstance } from "../api/axios";
 
 const router = Router();
 
@@ -27,11 +28,59 @@ router.post("/login", (req: Request, res: Response) => {
       );
 
       res.status(200).json({ message: "You have login successfully", token });
+      return;
     } else {
       res.status(404).json("Email or password is not valid");
+      return;
     }
   } else {
     res.status(404).json("Email or password is not valid");
+    return;
+  }
+});
+
+router.post("/register", async (req: Request, res: Response) => {
+  const { username, email, name, password, phone }: RegisterUser = req.body;
+
+  if (!username || !email || !name || !password || !phone) {
+    res.status(404).json("Please complete the register form");
+    return;
+  }
+
+  const users: User[] = db.get("users").value();
+
+  const checkEmail: User | undefined = users.find(
+    (user) => user.email === email
+  );
+  if (checkEmail) {
+    res.status(404).json("Someone already has this email, please try another");
+    return;
+  }
+
+  const checkUsername: User | undefined = users.find(
+    (user) => user.username === username
+  );
+  if (checkUsername) {
+    res
+      .status(404)
+      .json("Someone already has this username, please try another");
+    return;
+  }
+
+  try {
+    await axiosInstance.post("/users", {
+      username,
+      email,
+      name,
+      password,
+      phone,
+    });
+    res.status(200).json("You've been registred successfully");
+    return;
+  } catch (error) {
+    console.error("Something went wrong", error);
+    res.status(500).json("An error occurred during registration");
+    return;
   }
 });
 
--- server/src/types/userTypes.ts
+++ server/src/types/userTypes.ts
@@ -27,3 +27,11 @@ export type LoginUser = {
   email: string;
   password: string;
 };
+
+export type RegisterUser = {
+  username: string;
+  email: string;
+  name: string;
+  password: string;
+  phone: string;
+};
