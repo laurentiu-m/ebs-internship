186cff6eaa80f3b7e88ce354a186ad9133cee8a3
Laurentiu Molesteanu <laurentiu.molesteanu@internal.ebs.md>
2025-01-06 15:54:43 +0200

    fix: improve ProtectedRoute

--- client/src/router/ProtectedRoute.tsx
+++ client/src/router/ProtectedRoute.tsx
@@ -1,6 +1,6 @@
 import { api } from '@api/index';
 import { useQuery } from '@tanstack/react-query';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Loading } from 'src/components/Loading';
 
@@ -12,7 +12,14 @@ export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const token = localStorage.getItem('accessToken');
   const navigate = useNavigate();
 
-  const { isLoading } = useQuery(['validUser', token], () => api.users.validUser(token), {
+  useEffect(() => {
+    if (!token) {
+      navigate('/login');
+      return;
+    }
+  });
+
+  const { isLoading, isError } = useQuery(['validUser', token], () => api.users.validUser(token), {
     retry: false,
     onError: () => {
       navigate('/login');
@@ -21,5 +28,7 @@ export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
 
   if (isLoading) return <Loading />;
 
-  return <>{children}</>;
+  if (!isError) {
+    return children;
+  }
 };
