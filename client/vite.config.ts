016018808aa54c0652288a70bbf7ae15c18128cf
Laurentiu Molesteanu <laurentiu.molesteanu@internal.ebs.md>
2025-01-13 11:51:11 +0200

    feat: add new colors and variables to project

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@styles': path.resolve(__dirname, 'src/styles')
+    }
+  }
 });
