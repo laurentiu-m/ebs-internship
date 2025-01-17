a7b8e20daf14774bdd2dfe2767468613930744f5
Laurentiu Molesteanu <laurentiu.molesteanu@internal.ebs.md>
2025-01-17 16:59:35 +0200

    style: fix

--- client/src/layouts/dashboard/Dashboard.tsx
+++ client/src/layouts/dashboard/Dashboard.tsx
@@ -14,8 +14,8 @@ export const Dashboard = () => {
       <h1>Dashboard</h1>
 
       {Roles.Admin === tokenData.role && <h1>Welcome back, admin</h1>}
-      {Roles.Moderator === tokenData?.role && <h1>Welcome back, moderator</h1>}
-      {Roles.User === tokenData?.role && <h1>Welcome back, user</h1>}
+      {Roles.Moderator === tokenData.role && <h1>Welcome back, moderator</h1>}
+      {Roles.User === tokenData.role && <h1>Welcome back, user</h1>}
     </div>
   );
 };
