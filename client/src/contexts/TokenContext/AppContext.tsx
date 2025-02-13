fffc56a4c00476926cee5aafbc9591b6be8e0650
Laurentiu Molesteanu <laurentiu.molesteanu@internal.ebs.md>
2025-02-13 11:30:34 +0200

    fix: delete unused states in app context

--- client/src/contexts/TokenContext/AppContext.tsx
+++ client/src/contexts/TokenContext/AppContext.tsx
@@ -1,17 +1,12 @@
 import { createContext, Dispatch, SetStateAction } from 'react';
 
-import { JwtPayload, ModalMode } from '@src/types';
+import { JwtPayload } from '@src/types';
 
 type AppContext = {
   tokenData: JwtPayload | null;
   setTokenData: Dispatch<SetStateAction<JwtPayload | null>>;
   isSidebarClosed: boolean;
   onToggleSidebar: () => void;
-  isModalOpen: boolean;
-  selectedCell: number | null;
-  onOpenModal: (mode?: ModalMode, id?: number) => void;
-  onCloseModal: () => void;
-  modalMode: ModalMode;
 };
 
 export const AppContext = createContext<AppContext | null>(null);
--- client/src/contexts/TokenContext/AppProvider.tsx
+++ client/src/contexts/TokenContext/AppProvider.tsx
@@ -1,42 +1,21 @@
 import { ReactNode, useState } from 'react';
 
-import { JwtPayload, ModalMode } from '@src/types';
+import { JwtPayload } from '@src/types';
 
 import { AppContext } from './AppContext';
 
 export const AppProvider = ({ children }: { children: ReactNode }) => {
   const [tokenData, setTokenData] = useState<JwtPayload | null>(null);
   const [isSidebarClosed, setIsSidebarClosed] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalMode, setModalMode] = useState<ModalMode | null>(null);
-  const [selectedCell, setSelectedCell] = useState<number | null>(null);
 
   const onToggleSidebar = () => setIsSidebarClosed((prev) => !prev);
-
-  const onOpenModal = (mode?: ModalMode, id?: number) => {
-    setSelectedCell(id ?? null);
-    setModalMode(mode ?? null);
-    setIsModalOpen(true);
-  };
-
-  const onCloseModal = () => {
-    setIsModalOpen(false);
-    setModalMode(null);
-    setSelectedCell(null);
-  };
-
   return (
     <AppContext.Provider
       value={{
         tokenData,
         setTokenData,
         isSidebarClosed,
-        onToggleSidebar,
-        isModalOpen,
-        selectedCell,
-        onOpenModal,
-        onCloseModal,
-        modalMode
+        onToggleSidebar
       }}
     >
       {children}
