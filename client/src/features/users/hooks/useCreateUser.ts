eea8475210b11a6c98ab632ae28b5fd1960c394d
Laurentiu Molesteanu <laurentiu.molesteanu@internal.ebs.md>
2025-02-20 10:09:13 +0200

    refactor: use onError rather than try catch block, add hooks types, use teneray operator instead of if-else

--- client/src/features/users/hooks/useCreateUser.ts
+++ client/src/features/users/hooks/useCreateUser.ts
@@ -1,8 +1,7 @@
 import { apiClient } from '@src/api';
-import { UserCreate, UserCreateForm } from '@src/types';
+import { UserCreateForm, UserHook } from '@src/types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
-import { UseFormSetError } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 
@@ -11,35 +10,26 @@ export const useCreateUser = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async ({
-      data,
-      setError
-    }: {
-      data: UserCreate;
-      setError: UseFormSetError<UserCreateForm>;
-      onClose: () => void;
-    }) => {
-      try {
-        await apiClient.users.create(data);
-        toast.success(t('notification.user_create'));
-      } catch (err) {
-        if (err instanceof AxiosError) {
-          const errData = err.response?.data;
-
-          if (errData.error === 'form_invalid') {
-            errData.fields.forEach((error: { field: keyof UserCreateForm; message: string }) => {
-              setError(error.field, { type: 'server', message: error.message });
-            });
-            throw new Error('Validation error');
-          }
-          setError(errData.field, { type: errData.type, message: errData.messageKey });
-        }
-        throw err;
-      }
+    mutationFn: async ({ data }: UserHook) => {
+      await apiClient.users.create(data);
+      toast.success(t('notification.user_create'));
     },
     onSuccess: (_, { onClose }) => {
       queryClient.invalidateQueries({ queryKey: ['user_table'] });
       onClose();
+    },
+    onError: (err, { setError }) => {
+      if (err instanceof AxiosError) {
+        const errData = err.response?.data;
+
+        if (errData.error === 'form_invalid') {
+          errData.fields.forEach((error: { field: keyof UserCreateForm; message: string }) => {
+            setError(error.field, { type: 'server', message: error.message });
+          });
+          throw new Error('Validation error');
+        }
+        setError(errData.field, { type: errData.type, message: errData.messageKey });
+      }
     }
   });
 };
--- client/src/features/users/hooks/useEditUser.ts
+++ client/src/features/users/hooks/useEditUser.ts
@@ -1,8 +1,7 @@
 import { apiClient } from '@src/api';
-import { UserCreate, UserCreateForm } from '@src/types';
+import { UserCreateForm, UserHook } from '@src/types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
-import { UseFormSetError } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 
@@ -11,39 +10,27 @@ export const useEditUser = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async ({
-      userId,
-      data,
-      setError
-    }: {
-      userId: number;
-      data: UserCreate;
-      setError: UseFormSetError<UserCreateForm>;
-      onClose: () => void;
-    }) => {
-      try {
-        await apiClient.users.update(userId, data);
-        toast.success(t('notification.user_edit'));
-      } catch (err) {
-        if (err instanceof AxiosError) {
-          const errData = err.response?.data;
-
-          if (errData.error === 'form_invalid') {
-            errData.fields.forEach((error: { field: keyof UserCreateForm; message: string }) => {
-              setError(error.field, { type: 'server', message: error.message });
-            });
-            return;
-          }
-          setError(errData.field, { type: errData.type, message: errData.messageKey });
-        }
-
-        throw err;
-      }
+    mutationFn: async ({ userId, data }: UserHook) => {
+      await apiClient.users.update(userId!, data);
+      toast.success(t('notification.user_edit'));
     },
     onSuccess: async (_, { userId, onClose }) => {
       queryClient.invalidateQueries({ queryKey: [`edit_user_${userId}`] });
       queryClient.invalidateQueries({ queryKey: ['user_table'] });
       onClose();
+    },
+    onError: (err, { setError }) => {
+      if (err instanceof AxiosError) {
+        const errData = err.response?.data;
+
+        if (errData.error === 'form_invalid') {
+          errData.fields.forEach((error: { field: keyof UserCreateForm; message: string }) => {
+            setError(error.field, { type: 'server', message: error.message });
+          });
+          return;
+        }
+        setError(errData.field, { type: errData.type, message: errData.messageKey });
+      }
     }
   });
 };
